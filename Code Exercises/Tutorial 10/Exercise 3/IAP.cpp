0bd32e3fc5a24b82e422e504e984de91d3e7aead
sam_2 <samuel.hunt@uwe.ac.uk>
2019-03-27 10:40:34 +0000

    Adding files

---
+++ Code Exercises/Tutorial 10/Exercise 3/IAP.cpp
@@ -0,0 +1,40 @@
+void IAP::run ()
+{
+
+    while (true) {
+    
+        synth.playNote(slider1, 127);
+        aserveSleep(300);
+        synth.playNote(slider2, 127);
+        aserveSleep(300);
+        synth.playNote(slider3, 127);
+        aserveSleep(300);
+        synth.playNote(slider4, 127);
+        aserveSleep(300);
+        
+    }
+}
+
+
+void IAP::callbackCCValueChanged (int cc, int value)
+{
+    if (cc >= 41 && cc <= 44) {
+        switch (cc) {
+            case 41:
+                slider1 = value;
+                break;
+            case 42:
+                slider2 = value;
+                break;
+            case 43:
+                slider3 = value;
+                break;
+            case 44:
+                slider4 = value;
+                break;
+            default:
+                break;
+        }
+        
+    }
+}
