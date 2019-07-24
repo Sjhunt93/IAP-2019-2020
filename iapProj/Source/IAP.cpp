cbf7b84052686f8ec0adc6b5f99e463c672dab29
sam_2 <samuel.hunt@uwe.ac.uk>
2019-07-24 16:51:23 +0100

    adding solutions folders
    added a blank help file

--- iapProj/Source/IAP.cpp
+++ iapProj/Source/IAP.cpp
@@ -9,17 +9,8 @@
 #include "IAP.h"
 #include <iostream>
 
-float IAP::mtof (int note)
-{
-    return 440.0 * pow(2.0, (note - 69) / 12.0);
-}
-
-
 void IAP::run ()
 {
-    for (int i = 0; i < 100; i++) {
-        aserveOscillator(i % 24, arc4random() % 500 + 100, 0.1, 1);
-        aserveSleep(100);
-    }
+    
 }
 
