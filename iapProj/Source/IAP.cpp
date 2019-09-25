dbfa5ffe0fffb899fc695ed431c519df4fab0fff
sam_2 <samuel.hunt@uwe.ac.uk>
2019-09-25 14:41:57 +0100

    Project sets current working directory on startup
    This is to fix automated unit testing

--- iapProj/Source/IAP.cpp
+++ iapProj/Source/IAP.cpp
@@ -11,13 +11,6 @@
 
 void IAP::run ()
 {
-    while (true) {
-        aserveSleep(1000);
-    }
+    
 }
 
-void IAP::callbackNoteReceived (int note, int velocity, int channel)
-{
-    float f = 440.0 * pow(2.0, ((note-69.0) / 12.0));
-    aserveOscillator(0, f, 1.0, 0);
-}
